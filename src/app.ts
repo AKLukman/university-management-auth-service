682a3b7a3978c9240812b355566dc1587c42fab4
AKLukman <abulkashemlukman@gmail.com>
2023-06-26 20:44:49 +0100

    done

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
+import usersRouter from '../src/app/modules/users/users.route'
 
 const app: Application = express()
 
@@ -10,7 +11,10 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.get('/', (req: Request, res: Response) => {
+// Applications routes
+app.use('/api/v1/users', usersRouter)
+
+app.get('/', async (req: Request, res: Response) => {
   res.send('database working!')
 })
 
